ce1c41e0b9d684b090bc2694ee225bf9057ab46d
dark-fahim <itsmunna1066@gmail.com>
2025-01-30 21:03:37 +0600

    utility js added

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
+// function play(){
+//     const homeScreen = document.getElementById('home-screen')
+//     homeScreen.classList.remove('flex')
+//     homeScreen.classList.add('hidden')
+//     const playGround = document.getElementById('play-ground')
+//     playGround.classList.remove('hidden')
+// }
+
 function play(){
-    const homeScreen = document.getElementById('home-screen')
-    homeScreen.classList.remove('flex')
-    homeScreen.classList.add('hidden')
-    const playGround = document.getElementById('play-ground')
-    playGround.classList.remove('hidden')
+    hideElementById('home-screen')
+    showElementById('play-ground')
 }
+
